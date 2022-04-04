71181663ca2c6800136f25fb4f4ffc3a4e4f893b
Neeteesh Kumar <neetee@amazon.com>
2022-04-04 10:41:14 +0530

    Basic C++ Setup for CP

---
+++ solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF (int)1e9
+
+/*********************************************************/
+
+void solve(int test) {
+}
+
+/******************  Main Function  ***********************/
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int testCase;
+	cin >> testCase;
+	for (int test = 1; test <= testCase; test++) {
+		solve(test);
+	}
+	return 0;
+}
